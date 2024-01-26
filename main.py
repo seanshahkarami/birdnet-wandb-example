47f9b581526495338788b93e8d4182e2ae8934d6
Sean Shahkarami <sean.shahkarami@gmail.com>
2024-01-25 22:38:20 -0600

    move to sounddevice

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from birdnetlib.analyzer import Analyzer
 from datetime import datetime
 from pathlib import Path
 import soundfile as sf
-import soundcard as sc
+import sounddevice as sd
 from dataclasses import dataclass
 from pathlib import Path
 import wandb
@@ -15,30 +15,7 @@ class Args:
     file: Path | None
 
 
-def run(args: Args):
-    wandb.init(
-        # set the wandb project where this run will be logged
-        project="bird-detection",
-        # track hyperparameters and run metadata
-        config={
-            "architecture": "CNN",
-            "dataset": "CIFAR-100",
-            "epochs": 10,
-        },
-    )
-
-    print("loading analyzer model")
-    analyzer = Analyzer()
-
-    if args.file:
-        print("reading audio from file", args.file)
-        data, samplerate = sf.read(args.file)
-    else:
-        print("recording 30s of audio from default mic")
-        mic = sc.default_microphone()
-        samplerate = 48000
-        data = mic.record(samplerate=samplerate, numframes=30 * samplerate)
-
+def analyze_and_publish_detections(analyzer, data, samplerate):
     print("analyzing audio")
     recording = RecordingBuffer(
         analyzer,
@@ -47,26 +24,59 @@ def run(args: Args):
         # lat=35.4244,
         # lon=-120.7463,
         date=datetime.now(),
-        min_conf=0.25,
+        min_conf=wandb.config["min_conf"],
     )
     recording.analyze()
 
     print("printing detections")
+    detections_table = wandb.Table(["audio", "label", "confidence"])
     for i, d in enumerate(recording.detections):
         print("detection", i, d)
-        # {'common_name': 'American Goldfinch', 'scientific_name': 'Spinus tristis', 'start_time': 3.0, 'end_time': 6.0, 'confidence': 0.647074818611145, 'label': 'Spinus tristis_American Goldfinch'}
         start = int(samplerate * d["start_time"])
         end = int(samplerate * d["end_time"])
         segment = data[start:end]
-        # name
-        # outpath = Path(
-        #     "outputs",
-        #     path.name,
-        #     f"{d['label']}_{d['start_time']}_{d['end_time']}_{d['confidence']}.ogg",
-        # )
-        # outpath.parent.mkdir(exist_ok=True, parents=True)
-        # print("saving segment to", outpath)
-        # sf.write(str(outpath), segment, samplerate, format="ogg", subtype="vorbis")
+        detections_table.add_data(
+            wandb.Audio(segment, samplerate), d["label"], d["confidence"]
+        )
+
+    wandb.log({"detections": detections_table})
+
+
+def run(args: Args):
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="bird-detection",
+        # track hyperparameters and run metadata
+        config={
+            "min_conf": 0.25,
+            # "architecture": "CNN",
+            # "dataset": "CIFAR-100",
+            # "epochs": 10,
+        },
+    )
+
+    print("loading analyzer model")
+    analyzer = Analyzer()
+
+    # perform oneshot analysis for file input
+    if args.file:
+        print("reading audio from file", args.file)
+        data, samplerate = sf.read(args.file)
+        analyze_and_publish_detections(analyzer, data, samplerate)
+        return
+
+    while True:
+        print("recording 60s of audio from default mic")
+        duration = 60.0  # seconds
+        samplerate = 48000
+        data = sd.rec(
+            int(duration * samplerate),
+            samplerate=samplerate,
+            channels=1,
+            blocking=True,
+        )[:, 0]
+        sd.wait()
+        analyze_and_publish_detections(analyzer, data, samplerate)
 
 
 if __name__ == "__main__":
